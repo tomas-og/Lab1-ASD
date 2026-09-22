b43b591cc39e9c5acb4a28366fe08578150fc22a
Tomas Og Mac Donncha <tomasogmacdonncha04@gmail.com>
2026-09-22 19:50:15 +0100

    vernum

--- src/lab1_ASD/VernamCypher.java
+++ src/lab1_ASD/VernamCypher.java
@@ -1,11 +1,24 @@
 package lab1_ASD;
 
-import java.security.Key;
+public class VernamCypher extends AbstractCypher {
 
-public class VernamCypher extends AbstractCypher{
+    private String key;
 
-	
-	public void setKey(String key) {
-	}
+    public void setKey(String key) {
+        this.key = key;
+    }
 
+    public byte[] encrypt(byte[] plaintext) {
+        byte[] result = new byte[plaintext.length];
+
+        for (int i = 0; i < plaintext.length; i++) {
+            result[i] = (byte) (plaintext[i] ^ key.charAt(i % key.length()));
+        }
+
+        return result;
+    }
+
+    public byte[] decrypt(byte[] ciphertext) {
+        return encrypt(ciphertext);
+    }
 }
